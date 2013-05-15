85810191b68426441921e11c9c4ffc1068b25845
Willyam Surya <Willyam Surya@WillyamSurya-PC>
2013-05-16 00:36:13 +0700

    kategori maintenance service java

--- TokoGame/src/com/tokogame/service/KategoriMaintenanceServiceImpl.java
+++ TokoGame/src/com/tokogame/service/KategoriMaintenanceServiceImpl.java
@@ -9,8 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.tokogame.dao.KategoriDAO;
+import com.tokogame.dao.KoleksiDAO;
+import com.tokogame.dao.KoleksiRelationDAO;
 import com.tokogame.domain.Kategori;
 import com.tokogame.domain.KategoriExample;
+import com.tokogame.domain.Koleksi;
+import com.tokogame.domain.KoleksiExample;
+import com.tokogame.domain.KoleksiRelation;
+import com.tokogame.domain.KoleksiRelationExample;
 
 /**
  * @author mardy jonathan
@@ -21,6 +27,12 @@ public class KategoriMaintenanceServiceImpl implements KategoriMaintenanceServic
 
 	@Autowired
 	private KategoriDAO kategoriDAO;
+	
+	@Autowired
+	private KoleksiDAO koleksiDAO;
+	
+	@Autowired
+	private KoleksiRelationDAO koleksiRelationDAO;
 	
 	@Override
 	public List<Kategori> getKategoriList(Kategori kategori) {
@@ -55,8 +67,64 @@ public class KategoriMaintenanceServiceImpl implements KategoriMaintenanceServic
 		int pkKategori = kategori.getPkKategori();
 		kategoriDAO.deleteByPrimaryKey(pkKategori);
 	}
-	
-	
 
-	
+	@Override
+	public List<Koleksi> getKoleksiList(Koleksi koleksi) {
+		// TODO Auto-generated method stub
+		KoleksiExample koleksiExample = new KoleksiExample();
+		KoleksiExample.Criteria criteria = koleksiExample.createCriteria();
+		if(koleksi.getPkKoleksi()!=null){
+			criteria.andPkKoleksiEqualTo(koleksi.getPkKoleksi());
+		}
+		if(koleksi.getKoleksiName()!=null){
+			criteria.andKoleksiNameEqualTo(koleksi.getKoleksiName());
+		}
+		koleksiExample.setOrderByClause("koleksi_name asc");
+		return koleksiDAO.selectByExample(koleksiExample);
+	}
+
+	@Override
+	public void insertKoleksiRelation(KoleksiRelation koleksiRelation) {
+		// TODO Auto-generated method stub
+		koleksiRelationDAO.insert(koleksiRelation);
+	}
+
+	@Override
+	public void deleteKoleksiRelation(Integer fkKategori) {
+		// TODO Auto-generated method stub
+		KoleksiRelationExample koleksiRelationExample = new KoleksiRelationExample();
+		KoleksiRelationExample.Criteria criteria = koleksiRelationExample.createCriteria();
+		criteria.andFkKategoriEqualTo(fkKategori);
+		koleksiRelationDAO.deleteByExample(koleksiRelationExample);
+	}
+
+	public KoleksiRelationDAO getKoleksiRelationDAO() {
+		return koleksiRelationDAO;
+	}
+
+	public void setKoleksiRelationDAO(KoleksiRelationDAO koleksiRelationDAO) {
+		this.koleksiRelationDAO = koleksiRelationDAO;
+	}
+
+	@Override
+	public int countKategoriName(String kategoriName) {
+		// TODO Auto-generated method stub
+		KategoriExample kategoriExample = new KategoriExample();
+		KategoriExample.Criteria criteria = kategoriExample.createCriteria();
+		if (kategoriName!=null){
+			criteria.andKategoriNameEqualTo(kategoriName);
+		}
+		return kategoriDAO.countByExample(kategoriExample);
+	}
+
+	@Override
+	public List<KoleksiRelation> selectSelectiveKoleksiRelation(
+			Integer fkKategori) {
+		// TODO Auto-generated method stub
+		KoleksiRelationExample koleksiRelationExample = new KoleksiRelationExample();
+		KoleksiRelationExample.Criteria criteria = koleksiRelationExample.createCriteria();
+		criteria.andFkKategoriEqualTo(fkKategori);
+		//koleksiRelationExample.setOrderByClause("kategori_name asc");
+		return koleksiRelationDAO.selectByExample(koleksiRelationExample);
+	}	
 }
